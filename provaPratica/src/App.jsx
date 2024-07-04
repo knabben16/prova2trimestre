2959ca536873fd07cd8cdc45b39054f68db49521
VINÍCIUS KNABBEN ANDRADE <vinicius_andrade3@estudante.sesisenai.org.br>
2024-07-04 09:41:50 -0300

    prova2

---
+++ provaPratica/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Home from './components/Principal'
+
+function App() {
+
+  return (
+    <>
+     <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ provaPratica/src/components/Principal.jsx
@@ -0,0 +1,14 @@
+import Header from "./Cabeca"
+import Clubes from "./Lista"
+
+export default function Home(){
+return(
+<>
+<div className="App">
+<Header nome="Vinícius Knabben" Turma="Turma: 3B" />
+<Clubes />
+</div>
+</>
+)
+}
+
